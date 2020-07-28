cd619f4ec6468836fb4fb51e6407dceada2ae4a7
Joseph Calver <josephcalver@gmail.com>
2020-07-28 16:42:19 +0100

    Add DealPipelineController and Index view

---
+++ Equify/Controllers/DealPipelineController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Equify.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Equify.Controllers
+{
+    public class DealPipelineController : Controller
+    {
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            List<Deal> dealList = new List<Deal>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:8888/api/dealpipeline"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    dealList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Deal>>(apiResponse);
+                }
+            }
+
+                return View(dealList);
+        }
+    }
+}
---
+++ Equify/Models/Deal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Equify.Models
+{
+
+    public class Deal
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Company Name")]
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public int Founded { get; set; }
+
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string Country { get; set; }
+
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string Region { get; set; }
+
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string Sector { get; set; }
+
+        [Display(Name = "Enterprise Fair Value")]
+        [Range(1, 1000)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal EnterpriseValue { get; set; }
+
+        [Display(Name = "Deal Type")]
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string DealType { get; set; }
+
+        [Display(Name = "Deal Origination Date")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DealOriginationDate { get; set; }
+
+        [Display(Name = "Deal Status")]
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string DealStatus { get; set; }
+
+        [Display(Name = "Fund Investing")]
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string FundInvesting { get; set; }
+
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string Currency { get; set; }
+
+        [Display(Name = "Equity Required")]
+        [Required]
+        public int EquityRequired { get; set; }
+
+        [Display(Name = "Deal Team Lead")]
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string DealTeamLead { get; set; }
+
+        [Display(Name = "Investor Relations Lead")]
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string InvestorRelationsLead { get; set; }
+
+        [Display(Name = "ESG Lead")]
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string ESGLead { get; set; }
+    }
+}
